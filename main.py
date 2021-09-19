b124441c155f2e8fc2a2a80341cd9e22683408fb
YashDoshi01 <yashdoshi524@gmail.com>
2021-09-19 20:51:20 +0530

    quick update

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import kivy
 from kivy.app import App
-
+from kivy.uix.label import Label
 from kivy.uix.button import Button
 
 
 class LearnJava(App):
     def build(self):
-        return Button(text='Hello Word!')
+        return Label(text='Hello Word!')
 
 
 
